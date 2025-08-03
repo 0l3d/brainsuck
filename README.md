fec8d3bc85bb3bb2e72ef209da663e25f33d673c
0l3d <0l3dgit@gmail.com>
2025-08-03 16:26:39 +0300

    Compiler support

--- README.md
+++ README.md
@@ -1,27 +1,28 @@
 # Brainsuck 🧠
 
-A minimalist Brainfuck interpreter written in **C**
+A minimalist Brainfuck interpreter and compiler written in **C**
 
 ## Features
 
-* All 8 Brainfuck commands supported
-* 30,000 memory cells
-* File input/output
-* Interactive input with `,` command
-* Proper bracket matching for loops
+- All 8 Brainfuck commands supported
+- 30,000 memory cells
+- File input/output
+- Interactive input with `,` command
+- Proper bracket matching for loops
+- Compiles brainfuck code to FASM (x86_64 linux).
 
 ## Brainfuck Commands
 
-| Command | Description |
-|---------|-------------|
-| `+` | Increment memory cell |
-| `-` | Decrement memory cell |
-| `>` | Move pointer right |
-| `<` | Move pointer left |
-| `.` | Output character |
-| `,` | Input character |
-| `[` | Jump forward if cell is 0 |
-| `]` | Jump backward if cell is non-zero |
+| Command | Description                       |
+| ------- | --------------------------------- |
+| `+`     | Increment memory cell             |
+| `-`     | Decrement memory cell             |
+| `>`     | Move pointer right                |
+| `<`     | Move pointer left                 |
+| `.`     | Output character                  |
+| `,`     | Input character                   |
+| `[`     | Jump forward if cell is 0         |
+| `]`     | Jump backward if cell is non-zero |
 
 ## Installation
 
@@ -37,26 +38,11 @@ gcc -Os brainsuck.c -o brainsuck
 ./brainsuck <brainfuck_file>
 ```
 
-## Sample Programs
-
 ### Hello World (`hello.bf`)
 
 ```brainfuck
 ++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.
 ```
-
-### Game of Life (`life.bf`)
-
-Conway's Game of Life cellular automaton simulator with interactive interface.
-
-**How it works:**
-- Displays a 10x10 grid (expandable to 166x166) 
-- Use coordinates like "be" to toggle cell at column b, row e
-- Press Enter to advance one generation
-- Type "q" to quit
-- Grid wraps around (toroidal)
-- Follows standard Game of Life rules: cells live/die based on neighbor count
-- Living cells shown as `*`, dead cells as `-`
 
 ## Run Examples
 
@@ -70,5 +56,6 @@ Conway's Game of Life cellular automaton simulator with interactive interface.
 
 This project is licensed under the **GPL-3.0 License**.
 
----
-Created by **0l3d**
+## Author
+
+Created by **0l3d**.
--- brainsuck.c
+++ brainsuck.c
@@ -1,9 +1,82 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
-#include <stddef.h>
 
 #define MEMORY_SIZE 30000
 
+// MAIN COMPILER
+void create_fasm(char *code, FILE *out) {
+  int loop_id = 0;
+  int loop_stack[1000];
+  int stack_ptr = 0;
+
+  fprintf(out, "format ELF64 executable 3\n"
+               "segment readable writeable\n"
+               "tape rb 30000\n"
+               "segment readable executable\n"
+               "entry _start\n"
+               "_start:\n"
+               "lea rsi, [tape]\n");
+
+  for (int i = 0; code[i] != 0; i++) {
+    switch (code[i]) {
+    case '>':
+      fprintf(out, "inc rsi \n");
+      break;
+    case '<':
+      fprintf(out, "dec rsi\n");
+      break;
+    case '+':
+      fprintf(out, "inc byte [rsi]\n");
+      break;
+    case '-':
+      fprintf(out, "dec byte [rsi]\n");
+      break;
+    case '.':
+      fprintf(out, "mov rax, 1\n"
+                   "mov rdi, 1\n"
+                   "mov rdx, 1\n"
+                   "syscall\n");
+      break;
+    case ',':
+      fprintf(out, "mov rax, 0\n"
+                   "mov rdi, 0\n"
+                   "mov rdx, 1\n"
+                   "syscall\n");
+      break;
+
+    case '[': {
+      int current_id = loop_id++;
+      loop_stack[stack_ptr++] = current_id;
+      fprintf(out, "loop_start_%d_open:\n", current_id);
+      fprintf(out, "cmp byte [rsi], 0\n");
+      fprintf(out, "je loop_end_%d_close\n", current_id);
+      break;
+    }
+    case ']': {
+      if (stack_ptr <= 0) {
+        fprintf(stderr, "Unmatched ] %d\n", i);
+        return;
+      }
+      int current_id = loop_stack[--stack_ptr];
+      fprintf(out, "cmp byte [rsi], 0\n");
+      fprintf(out, "jne loop_start_%d_open\n", current_id);
+      fprintf(out, "loop_end_%d_close:\n", current_id);
+      break;
+    }
+    }
+  }
+
+  if (stack_ptr != 0) {
+    fprintf(stderr, "Unmatched [\n");
+    return;
+  }
+  fprintf(out, "mov rax, 60\n"
+               "xor rdi, rdi\n"
+               "syscall\n");
+}
+
+// MAIN INTERPRETER
 struct Brain {
   unsigned char memory[MEMORY_SIZE];
   size_t whereis;
@@ -16,42 +89,49 @@ struct Brain brainsucker(void) {
   return b;
 }
 
-
-void suck_in(struct Brain* br) {
+void suck_in(struct Brain *br) {
   br->memory[br->whereis] = (br->memory[br->whereis] + 1) % 256;
 }
-void suck_de(struct Brain* br) {
+void suck_de(struct Brain *br) {
   br->memory[br->whereis] = (br->memory[br->whereis] - 1) % 256;
 }
-void suck_move_right(struct Brain* br) {
+void suck_move_right(struct Brain *br) {
   if (br->whereis < MEMORY_SIZE - 1) {
     br->whereis++;
+    {
+    }
   }
 }
-void suck_move_left(struct Brain* br) {
+void suck_move_left(struct Brain *br) {
   if (br->whereis > 0) {
     br->whereis--;
   }
 }
-unsigned char get_current(struct Brain* br) {
-  return br->memory[br->whereis];
-}
-void set_data(struct Brain* br, unsigned char data) {
+unsigned char get_current(struct Brain *br) { return br->memory[br->whereis]; }
+void set_data(struct Brain *br, unsigned char data) {
   br->memory[br->whereis] = data;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   if (argc < 2) {
-    printf("Usage: %s <brainfck_file>", argv[0]);
+    printf("Usage: %s <brainfck_file> | [-c] compile code to fasm", argv[0]);
     return 1;
   }
 
-  char* path = argv[1];
-  FILE* file = fopen(path, "r");
+  int compile = 1;
+
+  FILE *outfasm = fopen("compiled.brainsuck.asm", "w");
+  if (outfasm == NULL) {
+    perror("create fasm open file errro");
+    return -1;
+  }
+
+  char *path = argv[1];
+  FILE *file = fopen(path, "r");
   struct Brain brainsuck = brainsucker();
 
   if (file == NULL) {
-    printf("Error: Could not open file\n");
+    printf("openfile error\n");
     return 1;
   }
 
@@ -67,9 +147,16 @@ int main(int argc, char* argv[]) {
       j++;
     }
   }
-
   fclose(file);
 
+  if (argc > 2) {
+    if (strcmp(argv[2], "-c") == 0) {
+      create_fasm(code, outfasm);
+    }
+  } else {
+    remove("compiled.brainsuck.asm");
+  }
+  fclose(outfasm);
   int i = 0;
   while (i < code_len) {
     switch (code[i]) {
@@ -96,39 +183,39 @@ int main(int argc, char* argv[]) {
       break;
     case '[':
       if (get_current(&brainsuck) == 0) {
-	int bracket_count = 1;
-	while (bracket_count > 0 && i < code_len - 1) {
-	  i++;
-	  switch (code[i]) {
-	  case '[':
-	    bracket_count++;
-	    break;
-	  case ']':
-	    bracket_count--;
-	    break;
-	  }
-	}
+        int bracket_count = 1;
+        while (bracket_count > 0 && i < code_len - 1) {
+          i++;
+          switch (code[i]) {
+          case '[':
+            bracket_count++;
+            break;
+          case ']':
+            bracket_count--;
+            break;
+          }
+        }
       }
       break;
     case ']':
       if (get_current(&brainsuck) != 0) {
-	int bracket_count = 1;
-	while (bracket_count > 0 && i > 0) {
-	  i--;
-	  switch (code[i]) {
-	  case ']':
-	    bracket_count++;
-	    break;
-	  case '[':
-	    bracket_count--;
-	    break;
-	  }
-	}
+        int bracket_count = 1;
+        while (bracket_count > 0 && i > 0) {
+          i--;
+          switch (code[i]) {
+          case ']':
+            bracket_count++;
+            break;
+          case '[':
+            bracket_count--;
+            break;
+          }
+        }
       }
       break;
     }
     i++;
   }
-  
+
   return 0;
 }
